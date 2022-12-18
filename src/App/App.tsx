65dc8da97a948e10c84d4e5322c25166ae8b5b9b
S.Botchorishvili <S.Botchorishvili@tbcpay.ge>
2022-12-18 14:04:23 +0400

    all the api call logic done and styling done

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import styled from "styled-components";
 import BackgroundImageDay from "../images/bg-image-daytime.jpg";
+import BackgroundImageDayTablet from "../images/bg-image-daytime-tablet.jpg";
+import BackgroundImageDayMobile from "../images/bg-image-daytime-mobile.jpg";
+import BackgroundImageNight from "../images/bg-image-nighttime.jpg";
+import BackgroundImageNightTablet from "../images/bg-image-nighttime-tablet.jpg";
+import BackgroundImageNightMobile from "../images/bg-image-nighttime-mobile.jpg";
 import { ReactComponent as IconRefresh } from "../images/icon-refresh.svg";
 import { ReactComponent as IconSun } from "../images/icon-sun.svg";
+import { ReactComponent as IconMoon } from "../images/icon-moon.svg";
 import { ReactComponent as IconArrowDown } from "../images/icon-arrow-down.svg";
 import useAPI from "../components/useAPI";
 
 interface MoreProps {
   more: boolean;
+  dayTime?: boolean;
 }
 
 interface IQuote {
@@ -22,18 +29,168 @@ interface IQuote {
   _id: string;
 }
 
+interface ITime {
+  abbreviation: string;
+  client_ip: string;
+  datetime: string;
+  day_of_week: number;
+  day_of_year: number;
+  dst: boolean;
+  dst_from: number | null;
+  dst_offset: number;
+  dst_until: number | null;
+  raw_offset: number;
+  timezone: string;
+  unixtime: number;
+  utc_datetime: string;
+  utc_offset: string;
+  week_number: number;
+}
+
+interface IIP {
+  country_code: string;
+  country_name: string;
+  city: string;
+  postal: number | null;
+  latitude: number;
+  longitude: number;
+  IPv4: string;
+  state: string;
+}
+
+interface ILocation {
+  data: {
+    ip: string;
+    type: string;
+    range_type: {
+      type: string;
+      description: string;
+    };
+    connection: {
+      asn: number;
+      organization: string;
+      isp: string;
+    };
+    location: {
+      geonames_id: number;
+      latitude: number;
+      longitude: number;
+      zip: string;
+      continent: {
+        code: string;
+        name: string;
+        name_translated: string;
+      };
+      country: {
+        alpha2: string;
+        alpha3: string;
+        calling_codes: string[];
+        currencies: {
+          symbol: string;
+          name: string;
+          symbol_native: string;
+          decimal_digits: number;
+          rounding: number;
+          code: string;
+          name_plural: string;
+        }[];
+        emoji: string;
+        ioc: string;
+        languages: {
+          name: string;
+          name_native: string;
+        }[];
+        name: string;
+        name_translated: string;
+        timezones: string[];
+        is_in_european_union: boolean;
+        fips: string;
+        geonames_id: string;
+        hasc_id: string;
+        wikidata_id: string;
+      };
+      city: {
+        fips: string | null;
+        alpha2: string | null;
+        geonames_id: string;
+        hasc_id: string | null;
+        wikidata_id: string;
+        name: string;
+        name_translated: string;
+      };
+      region: {
+        fips: string;
+        alpha2: string;
+        geonames_id: string | null;
+        hasc_id: string;
+        wikidata_id: string | null;
+        name: string;
+        name_translated: string;
+      };
+    };
+    tlds: string[];
+    timezone: {
+      id: string;
+      current_time: string;
+      code: string;
+      is_daylight_saving: boolean;
+      gmt_offset: number;
+    };
+    security: {
+      is_anonymous: null;
+      is_bot: null;
+      is_known_attacker: null;
+      is_proxy: null;
+      is_spam: null;
+      is_tor: null;
+      proxy_type: null;
+      threat_score: null;
+    };
+    domains: {
+      count: null;
+      domains: [];
+    };
+  };
+}
+
+const IP_URL = "https://geolocation-db.com/json/";
+const GEO_API_KEY = "B8dqMCbfdc8Pmg4CiC5BfG7oVaadN3su6oSp0erS";
+const GEO_LOCATION_URL = `https://api.ipbase.com/v2/info?apikey=${GEO_API_KEY}&ip=1.1.1.1`;
 const QUOTE_URL = "https://api.quotable.io/random";
+const TIME_URL = "http://worldtimeapi.org/api/";
 
 const HeroWrapper = styled.div<MoreProps>`
-  height: ${(props) => (props.more ? "70vh" : "100vh")};
+  height: ${(props) => (props.more ? "auto" : "100vh")};
   padding: 5.6rem 16.5rem;
-  background-image: url(${BackgroundImageDay});
+
+  ${(props) =>
+    props.dayTime
+      ? `background-image: url(${BackgroundImageDay})`
+      : `background-image: url(${BackgroundImageNight})`};
+
   font-size: 1.8rem;
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
   color: #fff;
   transition: all 0.5s;
+
+  @media (max-width: 1100px) {
+    ${(props) =>
+      props.dayTime
+        ? `background-image: url(${BackgroundImageDayTablet})`
+        : `background-image: url(${BackgroundImageNightTablet})`};
+
+    padding: 5rem 6.5rem;
+  }
+  @media (max-width: 768px) {
+    ${(props) =>
+      props.dayTime
+        ? `background-image: url(${BackgroundImageDayMobile})`
+        : `background-image: url(${BackgroundImageNightMobile})`};
+
+    padding: 3.2rem 2.6rem;
+  }
 
   &::before {
     content: "";
@@ -50,16 +207,26 @@ const HeroTop = styled.div`
   position: relative;
   height: 100%;
   transition: all 0.5s;
+
+  @media (max-width: 1100px) {
+    flex-direction: column;
+  }
 `;
 const HeroLeft = styled.div`
   width: 54rem;
-
+  height: 100%;
   display: flex;
   flex-direction: column;
+  @media (max-width: 1100px) {
+    margin-bottom: 8rem;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
-const QuoteWrapper = styled.div`
-  display: flex;
+const QuoteWrapper = styled.div<MoreProps>`
+  display: ${(props) => (props.more ? "none" : "flex")};
   align-items: flex-start;
 `;
 
@@ -86,6 +253,10 @@ const GreetingWrapper = styled.div`
 const StyledSunIcon = styled(IconSun)`
   margin-right: 1rem;
 `;
+
+const StyledMoonIcon = styled(IconMoon)`
+  margin-right: 1rem;
+`;
 const GreetingText = styled.p`
   font-size: 2rem;
   letter-spacing: 4px;
@@ -95,6 +266,14 @@ const ClockText = styled.p`
   font-size: 20rem;
   font-weight: bold;
   letter-spacing: -6px;
+
+  @media (max-width: 768px) {
+    font-size: 13rem;
+  }
+
+  @media (max-width: 350px) {
+    font-size: 10rem;
+  }
 `;
 
 const ClockSpan = styled.span`
@@ -111,8 +290,11 @@ const LocationText = styled.p`
 const HeroRight = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
   margin-left: auto;
+  @media (max-width: 1100px) {
+    margin-left: 0;
+    margin-right: auto;
+  }
 `;
 
 const CTAButton = styled.button`
@@ -147,35 +329,96 @@ const CTAIcon = styled(IconArrowDown)`
 `;
 
 const Bottom = styled.div<MoreProps>`
-  height: 30vh;
+  height: ${(props) => (props.more ? "auto" : "30vh")};
   display: ${(props) => (props.more ? "grid" : "none")};
   background-color: #ddd;
   padding: 7.4rem 16.5rem;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 4.2rem;
+  grid-column-gap: 25rem;
+
+  @media (max-width: 1100px) {
+    padding: 5rem 6.5rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 6rem 2.6rem;
+    grid-row-gap: 8.5rem;
+  }
+`;
+
+const InfoContainer = styled.div`
+  @media (max-width: 768px) {
+    grid-column: 1 / -1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+const InfoHeader = styled.p`
+  font-size: 1.5rem;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+`;
+
+const InfoData = styled.p`
+  font-size: 5.6rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 `;
 
 function App() {
   const [more, setMore] = React.useState<boolean>(false);
 
-  const [data, setData] = useAPI<IQuote>(QUOTE_URL, null);
+  const [quote, , setQuote] = useAPI<IQuote>(QUOTE_URL, null);
 
+  const [time] = useAPI<ITime>(`${TIME_URL}ip`, null);
+
+  const [dayTime, setDayTime] = React.useState<boolean>(true);
+
+  const [IP] = useAPI<IIP>(IP_URL, null);
+
+  const [locationData, setUrl] = useAPI<ILocation>(undefined, null);
+
+  React.useEffect(() => {
+    if (IP) {
+      setUrl(`${GEO_LOCATION_URL}&ip=${IP.IPv4}`);
+    }
+  });
+
+  React.useEffect(() => {
+    if (time) {
+      const hours = new Date(time.datetime).getHours();
+
+      if (hours > 6 && hours < 20) {
+        setDayTime(true);
+      } else {
+        setDayTime(false);
+      }
+    }
+  }, [time]);
   const refetchQuote = async () => {
     const res = await fetch(QUOTE_URL);
     if (res.ok) {
       const data: IQuote = await res.json();
-      setData(data);
+      setQuote(data);
     }
   };
 
   return (
     <>
-      <HeroWrapper more={more}>
+      <HeroWrapper more={more} dayTime={dayTime}>
         <HeroTop>
           <HeroLeft>
-            <QuoteWrapper>
+            <QuoteWrapper more={more}>
               <div>
-                <p>{data ? data.content : "Loading Quote..."}</p>
+                <p>{quote ? quote.content : "Loading Quote..."}</p>
                 <QuoteAuthor>
-                  {data ? data.author : "Loading Author..."}
+                  {quote ? quote.author : "Loading Author..."}
                 </QuoteAuthor>
               </div>
               <QuoteButton onClick={refetchQuote}>
@@ -184,18 +427,31 @@ function App() {
             </QuoteWrapper>
             <TimeWrapper>
               <GreetingWrapper>
-                <StyledSunIcon />
-                <GreetingText>GOOD MORNING, IT’S CURRENTLY</GreetingText>
+                {dayTime ? <StyledSunIcon /> : <StyledMoonIcon />}
+
+                <GreetingText>
+                  {dayTime ? "GOOD MORNING" : "GOOD EVENING"}, IT’S CURRENTLY
+                </GreetingText>
               </GreetingWrapper>
               <ClockText>
-                11:37<ClockSpan>BTS</ClockSpan>
+                {time &&
+                  new Date(time.datetime).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                  })}
+                <ClockSpan>{time && time.abbreviation}</ClockSpan>
               </ClockText>
-              <LocationText>In London, UK</LocationText>
+              <LocationText>
+                In{" "}
+                {locationData &&
+                  `${locationData.data.location.city.name}, ${locationData.data.location.country.alpha2}`}
+              </LocationText>
             </TimeWrapper>
           </HeroLeft>
           <HeroRight>
             <CTAButton onClick={() => setMore(!more)}>
-              {more ? "More" : "Less"}
+              {more ? "Less" : "More"}
               <CTAIconContainer>
                 <CTAIcon more={more} />
               </CTAIconContainer>
@@ -203,7 +459,24 @@ function App() {
           </HeroRight>
         </HeroTop>
       </HeroWrapper>
-      <Bottom more={more}>Bla</Bottom>
+      <Bottom more={more}>
+        <InfoContainer>
+          <InfoHeader>Current Timezone</InfoHeader>
+          <InfoData>{time?.timezone}</InfoData>
+        </InfoContainer>
+        <InfoContainer>
+          <InfoHeader>Day of the Week</InfoHeader>
+          <InfoData>{time?.day_of_week}</InfoData>
+        </InfoContainer>
+        <InfoContainer>
+          <InfoHeader>Day of the Year</InfoHeader>
+          <InfoData>{time?.day_of_year}</InfoData>
+        </InfoContainer>
+        <InfoContainer>
+          <InfoHeader>Week Number</InfoHeader>
+          <InfoData>{time?.week_number}</InfoData>
+        </InfoContainer>
+      </Bottom>
     </>
   );
 }
--- src/components/useAPI.ts
+++ src/components/useAPI.ts
@@ -1,28 +1,31 @@
 import React from "react";
 
 const useAPI = <T>(
-  url: string,
+  initialUrl: string | null | undefined,
   initialData: T | null | undefined
 ): [
   T | null | undefined,
+  React.Dispatch<React.SetStateAction<string | null | undefined>>,
   React.Dispatch<React.SetStateAction<T | null | undefined>>
 ] => {
   const [data, setData] = React.useState(initialData);
-
-  const fetchData = async (url: string) => {
-    const res = await fetch(url);
-
-    if (res.ok) {
-      const data = await res.json();
-      setData(data);
-    }
-  };
+  const [url, setUrl] = React.useState(initialUrl);
 
   React.useEffect(() => {
-    fetchData(url);
+    const fetchData = async (url: string) => {
+      const res = await fetch(url);
+
+      if (res.ok) {
+        const data = await res.json();
+        setData(data);
+      }
+    };
+    if (url) {
+      fetchData(url);
+    }
   }, [url]);
 
-  return [data, setData];
+  return [data, setUrl, setData];
 };
 
 export default useAPI;
